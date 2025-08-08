04f06cb63b9b8d9814b3b78992cc4ad9dd38fbc1
Paul Okikijesu Afolabi <paulafolabi745@gmail.com>
2025-08-08 18:18:43 +0100

    data validation

--- src/main.py
+++ src/main.py
@@ -24,15 +24,33 @@ async def insert_book(book_request: BookRequest):
     return Books
 
 @app.get("/get_book_by_id/{id}")
-async def get_book_by_id(book_id: int):
+async def get_book_by_id(book_id: int = Path(gt=0)):
     for book in Books:
         if book.id == book_id:
             return book
     return {"No book found"}
 
 @app.get("/get_book_by_rating/{rating}")
-async def get_book_by_rating(book_rating: int):
+async def get_book_by_rating(book_rating: int = Path(gt=0)):
     for book in Books:
         if book.rating == book_rating:
             return book
+    return {"No book found"}
+
+@app.put("/update_a_book/{book_id}")
+async def update_a_book(book_id:int = Path(gt=0), book_request: BookRequest = Body()):
+    mapped_book = Book(**book_request.model_dump())
+    target_book = await get_book_by_id(book_id)
+    target_book.title = mapped_book.title
+    target_book.rating = mapped_book.rating
+    target_book.description = mapped_book.description
+    target_book.author = mapped_book.author
+    return target_book
+
+@app.delete("/delete_a_book/{id}")
+async def delete_a_book(book_id: int = Path(gt=0)):
+    for book in Books:
+        if book.id == book_id:
+            Books.remove(book)
+            return {"Success"}
     return {"No book found"}
