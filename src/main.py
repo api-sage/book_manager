895214270f881970e3d0f0e17ed57ece365fd370
Paul Okikijesu Afolabi <paulafolabi745@gmail.com>
2025-08-08 06:05:44 +0100

    model validations

--- src/main.py
+++ src/main.py
@@ -1,12 +1,17 @@
-from fastapi import FastAPI
-from models.commons.book import Book
-from models.requests import bookRequest
+from fastapi import *
+from models.commons.book import *
+from models.requests.bookRequest import *
 
 Books = [
     Book(1, "Computer Science", "Paul Afolabi", "Introduction to computer science", 1),
     Book(2, "Philosophy Science", "Paul Cook", "Philosophy and humans", 2),
     Book(3, "Arts", "Tim Cook", "Graffiti art", 3)
 ]
+
+def get_latest_book_id(book: Book):
+    book.id = 1 if len(Books) == 0 else Books[-1].id + 1
+    return book
+
 app = FastAPI()
 
 @app.get("/get_all_books")
@@ -14,7 +19,7 @@ async def get_all_books():
     return Books
 
 @app.post("/inser_a_book")
-async def insert_book(book_request: bookRequest.BookRequest):
-    book_to_insert = Book(**book_request.model_dump())
-    Books.append(book_to_insert)
+async def insert_book(book_request: BookRequest):
+    mapped_book = Book(**book_request.model_dump())
+    Books.append(get_latest_book_id(mapped_book))
     return Books
--- src/models/requests/bookRequest.py
+++ src/models/requests/bookRequest.py
@@ -1,9 +1,21 @@
-from pydantic import BaseModel
+from typing import Optional
+from numpy.ma.core import min_val, max_val
+from pydantic import *
 
 class BookRequest(BaseModel):
-    id: int
-    title: str
-    author: str
-    description: str
-    rating: int
+    id: Optional[int] = Field(description="Id is not required for book creation", default=None)
+    title: str = Field(min_length=1, max_length=100)
+    author: str = Field(min_length=1, max_length=100)
+    description: str = Field(min_length=1, max_length=1000)
+    rating: int = Field(gt=0, lt=6)
 
+    model_config = {
+        "json_schema_extra":{
+            "example":{
+                "title":"Book title",
+                "author":"Book author",
+                "description":"Book description",
+                "rating":1,
+            }
+        }
+    }
